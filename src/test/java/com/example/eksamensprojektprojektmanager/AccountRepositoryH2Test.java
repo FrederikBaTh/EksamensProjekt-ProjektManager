2df379b34dd20d491bd1293e79bcc62825fe0195
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-05-27 23:22:50 +0200

    Fix test

--- src/test/java/com/example/eksamensprojektprojektmanager/AccountRepositoryH2Test.java
+++ src/test/java/com/example/eksamensprojektprojektmanager/AccountRepositoryH2Test.java
@@ -29,7 +29,7 @@ public class AccountRepositoryH2Test {
 
     @Test
     void createUser() {
-        // Create a new Account object
+
         Account newAccount = new Account();
         newAccount.setUsername("testUser3");
         newAccount.setPassword("testPassword3");
@@ -40,34 +40,26 @@ public class AccountRepositoryH2Test {
         newAccount.setJobTitle(null);
         newAccount.setDescription(null);
 */
-        // Save the new account to the repository
         repository.save(newAccount);
 
-        // Retrieve the account from the repository
         Account retrievedAccount = repository.getUserById(3L);
 
-        // Verify that the retrieved account's username matches the new account's username
         assertEquals("testUser3", retrievedAccount.getUsername());
     }
 
     @Test
     void loginTest() {
-        // Check if a user with username "testUser1" and password "testPassword1" exists
         boolean isValidUser = repository.isValidUser("testUser1", "testPassword1");
 
-        // Verify that the user exists
         assertTrue(isValidUser);
     }
     @Test
     void loginFailedTest() {
-        // Test data - Non-existing user in the database
         String username = "nonExistingUser";
         String password = "nonExistingPassword";
 
-        // Check if a user with username "nonExistingUser" and password "nonExistingPassword" exists
         boolean isValidUser = repository.isValidUser(username, password);
 
-        // Verify that the user does not exist
         assertFalse(isValidUser);
     }
 
--- src/test/java/com/example/eksamensprojektprojektmanager/ProjectRepositoryH2Test.java
+++ src/test/java/com/example/eksamensprojektprojektmanager/ProjectRepositoryH2Test.java
@@ -24,7 +24,7 @@ public class ProjectRepositoryH2Test {
 
     @Test
     void createProjectTest() {
-        // Create a new Project object
+
         Project newProject = new Project();
         newProject.setProjectName("Test Project");
         newProject.setUserId(1L);
@@ -32,32 +32,26 @@ public class ProjectRepositoryH2Test {
         newProject.setProjectDeadline(LocalDate.of(2022, 12, 31));
         newProject.setProjectStatus("Active");
 
-        // Save the new project to the repository
         Project savedProject = repository.addProject(newProject);
 
-        // Verify that the project was saved successfully
         assertNotNull(savedProject);
         assertEquals("Test Project", savedProject.getProjectName());
     }
 
     @Test
     void editProjectTest() {
-        // Retrieve an existing project from the repository
+
         Project existingProject = repository.getProjectById(1L);
 
-        // Verify that the project was retrieved successfully
         assertNotNull(existingProject);
 
-        // Edit the project
         existingProject.setProjectName("Updated Project");
         existingProject.setStartDate(LocalDate.of(2023, 3, 22));
         existingProject.setProjectDeadline(LocalDate.of(2023, 6, 28));
         existingProject.setProjectStatus("Updated");
 
-        // Update the project in the repository
         Project updatedProject = repository.updateProject(existingProject);
 
-        // Verify that the project was updated successfully
         assertNotNull(updatedProject);
         assertEquals("Updated Project", updatedProject.getProjectName());
         assertEquals(LocalDate.of(2023, 3, 22), updatedProject.getStartDate());
@@ -67,24 +61,19 @@ public class ProjectRepositoryH2Test {
 
     @Test
     void deleteProjectTest() {
-        // Retrieve an existing project from the repository
+
         Project existingProject = repository.getProjectById(1L);
 
-        // Verify that the project was retrieved successfully
         assertNotNull(existingProject);
 
-        // Delete the project
         repository.deleteProjectById(existingProject.getProject_id());
 
-        // Try to retrieve the deleted project
         Project deletedProject = null;
         try {
             deletedProject = repository.getProjectById(existingProject.getProject_id());
         } catch (EmptyResultDataAccessException e) {
-            // This exception is expected because the project has been deleted
         }
 
-        // Verify that the project was deleted successfully
         assertNull(deletedProject);
     }
 
